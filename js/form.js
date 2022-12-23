b9116a31f5fbe683051e17ef944875b76a5ecdab
Eric <eric2umeh@yahoo.com>
2022-12-23 09:51:30 +0100

    Update the validation and style

--- js/form.js
+++ js/form.js
@@ -2,8 +2,8 @@ const email = document.querySelector('#email');
 const ErrorMsg = document.querySelector('.errorMsg');
 const contact = document.querySelector('#contacts');
 contact.addEventListener('submit', (event) => {
-    const regex = /[A-Z]/;
-    const emailValue = email.value;
+  const regex = /[A-Z]/;
+  const emailValue = email.value;
   if (regex.test(emailValue)) {
     ErrorMsg.innerHTML = 'Invalid! Your email should contain lowerscase letters';
     event.preventDefault();
