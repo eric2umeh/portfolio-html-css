f4b92e15f3c1670336f8f6ba07750bdc0500a34f
PrinceBadru <100031675+PrinceBadru@users.noreply.github.com>
2022-12-23 11:46:40 +0300

    Add regex validation

--- js/form.js
+++ js/form.js
@@ -2,7 +2,7 @@ const email = document.querySelector('#email');
 const ErrorMsg = document.querySelector('.errorMsg');
 const contact = document.querySelector('#contacts');
 contact.addEventListener('submit', (event) => {
-    // Badru here ....
+    const regex = /[A-Z]/;
     const emailValue = email.value;
   if (regex.test(emailValue)) {
     ErrorMsg.innerHTML = 'Invalid! Your email should contain lowerscase letters';
